b72c9e95426332edcd2126cd9f0af58b0b66b4ba
leysont <leysont@tutanota.com>
2023-11-09 08:24:58 +0100

    added todos

--- Program.cs
+++ Program.cs
@@ -2,19 +2,22 @@
 using System.Windows.Forms;
 using ThreeCee.Forms;
 
-namespace ThreeCee
+namespace ThreeCee;
+
+// TODO: Form: Add vehicle; connect to button
+// TODO: Form: Edit vehicle; connect to button
+// TODO: connect remove vehicle to button
+
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
-        }
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
     }
 }
